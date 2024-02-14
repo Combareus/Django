ef0fcebbf17620c3bb0c0b224c23a510369c0e38
Eric Song <ericyousong@gmail.com>
2024-02-14 12:39:50 -0500

    classes added

--- djangoapp/auth/views.py
+++ djangoapp/auth/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from classes import Schedule
+from classes import Employee
+from classes import Surgeon
+from classes import Cleaner
+from classes import Patient
+
 
 # Create your views here.
