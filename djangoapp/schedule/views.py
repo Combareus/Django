9a411ed72a2bccea41ddb5257533c77b196d4d3c
shougan <dashoug001@gmail.com>
2024-02-20 14:18:14 -0500

    url linking

--- djangoapp/schedule/views.py
+++ djangoapp/schedule/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# view function: request -> response (request handler)
+
+
+def say_hello(request):
+    '''
+    Testing View Function that is called at an url
+
+    '''
+    return HttpResponse("Hello world:)")
