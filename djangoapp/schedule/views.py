b56431da3d4bf5cb08997ef33edcdd821cd113d4
shougan <dashoug001@gmail.com>
2024-03-01 14:19:20 -0500

    docstrings/comments

--- djangoapp/schedule/views.py
+++ djangoapp/schedule/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from schedule.classes import Surgeon, Employee, Patient, Schedule, Cleaner
+from schedule.models import Surgeon, Employee, Patient, Schedule, Cleaner
 
 # Create your views here.
 # view function: request -> response (request handler)
@@ -17,6 +17,14 @@ def say_hello(request):
 
 
 def index(request):
+    '''
+    View function displays calls template to display current surgeons
+    Input:
+        request (HTML request)
+    Output:
+        render() (HTML file): note 
+    '''
+    surgeonlst = [Surgeon("John Doe", "Sr"), Surgeon("Smith", "Sr")] #list of surgeons to be linked with database
     return render(request, 'index.html',{
-        'Surgeons': [Surgeon("John Doe", "Sr"), Surgeon("Smith", "Sr")]
+        'Surgeons': surgeonlst
     })
