1994a61bebfad8830715b4268749bfd3f76502d8
KunyuanHu <khu1@ocdsb.ca>
2024-03-05 12:26:32 -0500

    started new apps

--- djangoapp/user_settings/apps.py
+++ djangoapp/user_settings/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyappConfig(AppConfig):
+class UserSettingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myApp'
+    name = 'user_settings'
